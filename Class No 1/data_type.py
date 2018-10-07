d0690671065f963433be1a5b3a8c97088c7599dc
MD AL-AMIN <38142928+thenimala@users.noreply.github.com>
2018-10-07 23:48:28 +0600

    Add files via upload

---
+++ Class No 1/data_type.py
@@ -0,0 +1,34 @@
+#Data Type
+#Numeric (int,float,complex,boolean)
+
+#int
+a=10
+print(a)
+type(a)
+
+A=23
+print(A)
+A=20
+print(A)
+type(A)
+print(float(a))
+
+#float
+float1=3.5
+print(float1)
+type(float1)
+print(int(float1))
+
+#complex
+
+com=3+3j
+print(com)
+type(com)
+
+#boolean
+
+temp_vari=True
+print(temp_vari)
+type(temp_vari)
+temp_vari2=False
+print(temp_vari2)
