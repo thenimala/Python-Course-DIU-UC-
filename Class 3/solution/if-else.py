1ae9daa6e3743900c25f26e3934ddd83993bfaef
MD AL-AMIN <38142928+thenimala@users.noreply.github.com>
2018-10-21 22:32:46 +0600

    Add files via upload

---
+++ Class 3/solution/if-else.py
@@ -0,0 +1,12 @@
+N = int(input("Enter a Number: "))
+
+if N % 2 != 0:
+    print ("Weird")
+else:
+    if N >= 2 and N <= 5:
+        print ("Not Weird")
+    elif N >= 6 and N <= 20:
+        print ("Weird")
+    elif N > 20:
+        print ("Not Weird")
+
